3e8f048b3b4a8c5cb39d9191963c6325b31e9fa0
b01tech <bruno01.tech@gmail.com>
2025-10-27 13:27:36 -0300

    refactor(Data): remove null-forgiving operator and add DbContext constructor

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -5,11 +5,12 @@ namespace BugStore.Data;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<Customer> Customers { get; set; } = null!;
-    public DbSet<Product> Products { get; set; } = null!;
-    public DbSet<Order> Orders { get; set; } = null!;
-    public DbSet<OrderLine> OrderLines { get; set; } = null!;
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderLine> OrderLines { get; set; }
 
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlite();
 }
