342f2b845fbf8b5be30e09f300eb299ec2d350d0
b01tech <bruno01.tech@gmail.com>
2025-10-27 15:23:34 -0300

    feat(Data): add entity relationships configuration in AppDbContext

--- src/Infra/Data/AppDbContext.cs
+++ src/Infra/Data/AppDbContext.cs
@@ -11,6 +11,33 @@ public class AppDbContext : DbContext
     public DbSet<OrderLine> OrderLines { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlite();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configurar relacionamento Order -> OrderLines
+        modelBuilder.Entity<Order>()
+            .HasMany(o => o.Lines)
+            .WithOne()
+            .HasForeignKey(ol => ol.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Configurar relacionamento OrderLine -> Product
+        modelBuilder.Entity<OrderLine>()
+            .HasOne(ol => ol.Product)
+            .WithMany()
+            .HasForeignKey(ol => ol.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Configurar relacionamento Order -> Customer
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Customer)
+            .WithMany()
+            .HasForeignKey(o => o.CustomerId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
